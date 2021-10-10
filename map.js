681cdd9ef17d915d85c12e3e71a7a0a42b34b5f3
Joanna Gorska <gorska919@gmail.com>
2021-10-10 11:45:35 +0000

    Add examples of use of map method

--- map.js
+++ map.js
@@ -6,15 +6,33 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num*2);
+}
+
+console.log(results)
 
 // Using map()
 
+const multByTwo = function (num) {
+  return num * 2;
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
+
+const mapResultsSimple = nums.map(function (num) { return num * 2 });
+console.log(mapResultsSimple);
 
 
 // Simplfied w/ map() + arrow function
 
+const mapResultsArrow = nums.map(num => num * 2 );
+console.log(mapResultsArrow);
 
 // With objects:
 const students = [
@@ -37,3 +55,8 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+
+let studentsWithIds = students.map(student => [student.name, student.id]);
+console.log("studnets with IDs", studentsWithIds)
+
